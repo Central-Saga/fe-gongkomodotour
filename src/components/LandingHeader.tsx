1f35756ae3e22d642582b525dc9425e625e919dd
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-03-12 00:22:30 +0800

    Update LandingHeader with logo and improved navigation layout

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -2,51 +2,57 @@
 
 import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuTrigger, NavigationMenuContent, NavigationMenuLink } from '@/components/ui/navigation-menu';
 import { GoPerson } from "react-icons/go";
+import Image from 'next/image';
+import logo from '../../public/img/logo.png';
 
 export default function LandingHeader() {
   return (
     <header className="bg-gray-100 border-b shadow-sm">
       <div className="container mx-auto flex justify-between items-center p-4">
         {/* Logo (Teks Sementara) */}
-        <div className="text-2xl font-bold text-gray-800">
-          Gong Komodo Tour
+        <div className="flex items-center">
+          <Image 
+            src={logo} 
+            alt="Gong Komodo Tour Logo" 
+            className="h-12 w-auto" 
+          />
         </div>
         {/* Navigation menu links */}
-        <NavigationMenu>
-          <NavigationMenuList className="flex space-x-4">
-            <NavigationMenuItem>
-              <NavigationMenuLink href="#">Home</NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="#">Packages</NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="#">Gallery</NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="#">Blog</NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="#">About Us</NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-
-        {/* Right side: Language dropdown and user logo */}
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-8">
           <NavigationMenu>
-            <NavigationMenuList>
+            <NavigationMenuList className="flex space-x-4">
               <NavigationMenuItem>
-                <NavigationMenuTrigger>Bahasa</NavigationMenuTrigger>
-                <NavigationMenuContent className="w-32">
-                  <NavigationMenuLink href="#">English</NavigationMenuLink>
-                  <NavigationMenuLink href="#">Bahasa Indonesia</NavigationMenuLink>
-                </NavigationMenuContent>
+                <NavigationMenuLink href="#">Home</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="#">Packages</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="#">Gallery</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="#">Blog</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="#">About Us</NavigationMenuLink>
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
-          <div>
-            <GoPerson className="w-8 h-8 rounded-full" />
+          <div className="flex items-center space-x-4">
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>Bahasa</NavigationMenuTrigger>
+                  <NavigationMenuContent className="w-32">
+                    <NavigationMenuLink href="#">English</NavigationMenuLink>
+                    <NavigationMenuLink href="#">Bahasa Indonesia</NavigationMenuLink>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+            <div>
+              <GoPerson className="w-8 h-8 rounded-full" />
+            </div>
           </div>
         </div>
       </div>
